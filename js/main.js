b91973f9a577e588e4d801318b49583d4dd960fb
Benjamin Mercer <mercerbe@gmail.com>
2018-12-23 17:32:47 -0500

    add easing, gitignore, images for testing

--- js/main.js
+++ js/main.js
@@ -58,3 +58,119 @@ var section2 = new ScrollMagic.Scene ({
 
 });
 
+//easing 
+//pagegination functions
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Closes the sidebar menu
+    $(".menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+        $(this).toggleClass("active");
+    });
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('#sidebar-wrapper .js-scroll-trigger').click(function () {
+        $("#sidebar-wrapper").removeClass("active");
+        $(".menu-toggle").removeClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+})(jQuery); // End of use strict
+
+//email contact form
+$(function () {
+    function after_form_submitted(data) {
+        if (data.result == 'success') {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        }
+        else {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors, function (key, val) {
+                $('#error_message ul').append('<li>' + key + ':' + val + '</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function () {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if (label) {
+                    $btn.prop('type', 'submit');
+                    $btn.text(label);
+                    $btn.prop('orig_label', '');
+                }
+            });
+
+        }//else
+    }
+
+    $('#reused_form').submit(function (e) {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function () {
+            $btn = $(this);
+            $btn.prop('type', 'button');
+            $btn.prop('orig_label', $btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+        $.ajax({
+            type: "POST",
+            url: 'handler.php',
+            data: $form.serialize(),
+            success: after_form_submitted,
+            dataType: 'json'
+        });
+
+    });
+});
+
+
+//instafeed
+var myFeed = new Instafeed({
+    get: 'user',
+    userId: '1256541053',
+    clientId: '1e9a77d650c149658deed38a4b379a0f',
+    accessToken: '1256541053.1e9a77d.dcb9b0d341ab458dbe5c6d10311af75f',
+    resolution: 'thumbnail',
+    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+    sortBy: 'most-recent',
+    limit: 8,
+    links: false
+});
+myFeed.run();
+
